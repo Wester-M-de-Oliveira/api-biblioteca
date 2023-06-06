2f4fbf4991da49ea6caa671de5d2ce8b861d3717
João Teixeira <joao.teixeira@ifro.edu.br>
2023-06-05 20:10:59 -0400

    operações: update, delete e show

--- src/api/components/despesa/despesa.controller.ts
+++ src/api/components/despesa/despesa.controller.ts
@@ -7,13 +7,13 @@ export class DespesaController {
 
     const despesas = await AppDataSource.manager.find(Despesa)
 
-    res.status(200).json({ dados: despesas });
+    return res.status(200).json({ dados: despesas, total: despesas.length });
   }
 
   public async create(req: Request, res: Response) {
-    
+
     //aqui que pegamos o dados para cadastrar uma nova despesa
-    
+
     // let descricao = req.body.descricao;
     // let valor = req.body.valor;
     // let data = req.body.data;
@@ -27,6 +27,60 @@ export class DespesaController {
 
     const _despesa = await AppDataSource.manager.save(desp);
 
-    res.status(201).json(_despesa);
+    return res.status(201).json(_despesa);
+  }
+
+  public async update(req: Request, res: Response) {
+
+    // const cod = req.params.cod;
+    const { cod } = req.params;
+
+    const despesa = await AppDataSource.manager.findOneBy(Despesa, { id: cod });
+
+    if (despesa == null) {
+      return res.status(404).json({ erro: 'Despesa não encontrada!' });
+    }
+
+    let { descricao, valor, data, data_efetivacao, valor_pago } = req.body;
+
+    despesa.descricao = descricao;
+    despesa.data = data;
+    despesa.data_efetivacao = data_efetivacao;
+    despesa.valor = valor;
+    despesa.valor_pago = valor_pago;
+
+    if (valor_pago >= despesa.valor) {
+      despesa.pago = true;
+    }
+
+    const despesa_salva = await AppDataSource.manager.save(despesa);
+
+    return res.json(despesa_salva);
+  }
+
+  public async destroy(req: Request, res: Response) {
+    const { cod } = req.params;
+
+    const despesa = await AppDataSource.manager.findOneBy(Despesa, { id: cod });
+
+    if (despesa == null) {
+      return res.status(404).json({ erro: 'Despesa não encontrada!' });
+    }
+
+    await AppDataSource.manager.delete(Despesa, despesa);
+
+    return res.status(204).json();
+  }
+
+  public async show(req: Request, res: Response) {
+    const { cod } = req.params;
+
+    const despesa = await AppDataSource.manager.findOneBy(Despesa, { id: cod });
+
+    if (despesa == null) {
+      return res.status(404).json({ erro: 'Despesa não encontrada!' });
+    }
+
+    return res.json(despesa);
   }
 }
